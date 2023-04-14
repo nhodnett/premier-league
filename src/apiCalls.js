57fc8de239118e66a0c464aa629d62f028fae33b
Nathan Hodnett <nathanhodnett@gmail.com>
2023-04-14 15:23:22 -0600

    Update API endpoint

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
 export const getData = () => {
-    return fetch(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League`)
+    return fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League`)
     .then(response => response.json())
 }
